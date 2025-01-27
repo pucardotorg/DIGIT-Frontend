1d6dfea88aa1165b2aa2295a81c76285c5661a78
rkum113 <raushan.k@beehyv.com>
2025-01-27 10:56:25 +0530

    minor changes

--- micro-ui/web/src/App.js
+++ micro-ui/web/src/App.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { initLibraries } from "@egovernments/digit-ui-libraries";
-import {
-  paymentConfigs,
-  PaymentLinks,
-  PaymentModule,
-} from "@egovernments/digit-ui-module-common";
 import { DigitUI } from "@egovernments/digit-ui-module-core";
-import { initDSSComponents } from "@egovernments/digit-ui-module-dss";
-import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
 import { initHRMSComponents } from "@egovernments/digit-ui-module-hrms";
-import { initUtilitiesComponents } from "@egovernments/digit-ui-module-utilities";
 import { UICustomizations } from "./Customisations/UICustomizations";
 import { initWorkbenchComponents } from "@egovernments/digit-ui-module-workbench";
+import { initUtilitiesComponents } from "@egovernments/digit-ui-module-utilities";
 import {
   initPGRComponents,
   PGRReducers,
 } from "@egovernments/digit-ui-module-pgr";
+import { initEngagementComponents } from "@egovernments/digit-ui-module-engagement";
 
 window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
 
@@ -27,7 +21,6 @@ const enabledModules = [
   "Engagement",
   "Workbench",
   "PGR"
-
 ];
 
 const moduleReducers = (initData) => ({
@@ -35,23 +28,16 @@ const moduleReducers = (initData) => ({
 });
 
 const initDigitUI = () => {
-  window.Digit.ComponentRegistryService.setupRegistry({
-    PaymentModule,
-    ...paymentConfigs,
-    PaymentLinks,
-  });
-
-  initPGRComponents();
-  initDSSComponents();
-  initHRMSComponents();
-  initEngagementComponents();
-  initUtilitiesComponents();
-  initWorkbenchComponents();
-
+  window.Digit.ComponentRegistryService.setupRegistry({});
   window.Digit.Customizations = {
     PGR: {},
     commonUiConfig: UICustomizations,
   };
+  initPGRComponents();
+  initEngagementComponents();
+  initHRMSComponents();
+  initUtilitiesComponents();
+  initWorkbenchComponents();
 };
 
 initLibraries().then(() => {
@@ -71,7 +57,7 @@ function App() {
       stateCode={stateCode}
       enabledModules={enabledModules}
       moduleReducers={moduleReducers}
-      // defaultLanding="employee"
+      defaultLanding="employee"
     />
   );
 }
